0fcc6d0c43cf266029b5dc4b5a4aacdb2c3d3fb1
David Chmarzynski <david.chm00@gmail.com>
2023-09-21 08:08:37 +0200

    feat(cutting-counter): add cutting counter & tomato slices prefab

--- Assets/_Assets/Scripts/BaseCounter.cs
+++ Assets/_Assets/Scripts/BaseCounter.cs
@@ -14,6 +14,11 @@ public class BaseCounter : MonoBehaviour, IKitchenObjectParent
         Debug.LogError("BaseCounter.Interact() overriding error !");
     }
 
+    public virtual void InteractAlternate(Player player)
+    {
+        Debug.LogError("BaseCounter.InteractAlternate() overriding error !");
+    }
+
     public Transform GetKitchenObjectFollowTransform()
     {
         return counterTopPoint;
