b2ea3de0b415fdedf62499b86624bfc1450cfede
David Chmarzynski <david.chm00@gmail.com>
2023-09-27 20:08:59 +0200

    feat(delivery-counter): implement arrow shaders

---
+++ Assets/_Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+
+    public override void Interact(Player player)
+    {
+        if(player.HasKitchenObject())
+        {
+            if(player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                // Only accept Plates
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}
