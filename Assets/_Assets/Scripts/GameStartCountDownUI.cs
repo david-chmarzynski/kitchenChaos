9d5d0623249da73d45b0e6b77d64b3bb20c142ef
David Chmarzynski <david.chm00@gmail.com>
2023-10-02 17:49:51 +0200

    feat(countDown): implement countDownToStart

---
+++ Assets/_Assets/Scripts/GameStartCountDownUI.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameStartCountDownUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI countDownText;
+
+    private void Start()
+    {
+        GameManager.Instance.OnStateChanged += GameManager_OnStateChanged;
+
+        Hide();
+    }
+
+    private void Update()
+    {
+        countDownText.text = Mathf.Ceil(GameManager.Instance.GetCountDownToStartTimer()).ToString();
+    }
+
+    private void GameManager_OnStateChanged(object sender, System.EventArgs e)
+    {
+        if(GameManager.Instance.IsCountDownToStartActive())
+        {
+            Show();
+        } else
+        {
+            Hide();
+        }
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false); 
+    }
+}
