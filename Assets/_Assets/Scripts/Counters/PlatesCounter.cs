3ca9a0993be44fb0d0250f02c236c968d42323e3
David Chmarzynski <david.chm00@gmail.com>
2023-10-11 17:23:10 +0200

    feat(polish): add tutorial manager + fix game start on it

--- Assets/_Assets/Scripts/Counters/PlatesCounter.cs
+++ Assets/_Assets/Scripts/Counters/PlatesCounter.cs
@@ -20,7 +20,7 @@ public class PlatesCounter : BaseCounter
         if(spawnPlateTimer > spawnPlateTimerMax)
         {
             spawnPlateTimer = 0f;
-            if(platesSpawnedAmount < platesSpawnedAmountMax)
+            if(GameManager.Instance.IsGamePlaying() && platesSpawnedAmount < platesSpawnedAmountMax)
             {
                 platesSpawnedAmount++;
 
--- Assets/_Assets/Scripts/GameInput.cs
+++ Assets/_Assets/Scripts/GameInput.cs
@@ -9,6 +9,7 @@ public class GameInput : MonoBehaviour {
     public event EventHandler OnInteractAction;
     public event EventHandler OnInteractAlternateAction;
     public event EventHandler OnPauseAction;
+    public event EventHandler OnBindingRebind;
     private const string PLAYER_PREFS_BINDINGS = "InputBindings";
 
     public static GameInput Instance { get; private set; }
@@ -162,6 +163,7 @@ public class GameInput : MonoBehaviour {
             onActionRebound();
             PlayerPrefs.SetString(PLAYER_PREFS_BINDINGS, playerInputActions.SaveBindingOverridesAsJson());
             PlayerPrefs.Save();
+            OnBindingRebind?.Invoke(this, EventArgs.Empty);
         }).Start();
     }
 }
